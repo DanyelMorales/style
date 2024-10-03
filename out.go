c503d66105db82ee660d58811a9da45cdb75c36d
danielvera <danyelmorales1991@gmail.com>
2024-10-03 16:43:58 -0700

    support view

--- out.go
+++ out.go
@@ -2,9 +2,79 @@ package style
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"strings"
 )
+
+func ReportVF(w io.Writer, format string, a ...interface{}) {
+	LogViewF(w, Documentation, format, a...)
+}
+
+func ReportV(w io.Writer, format string) {
+	LogViewF(w, Documentation, format)
+}
+
+func NoticeVF(w io.Writer, format string, a ...interface{}) {
+	LogViewF(w, Notice, format, a...)
+}
+
+func NoticeV(w io.Writer, format string) {
+	LogViewF(w, Notice, format)
+}
+
+func TitleV(w io.Writer, title string) {
+	LogViewF(w, AddonEnable, title)
+}
+
+func TitleVF(w io.Writer, format string, a ...interface{}) {
+	LogViewF(w, AddonEnable, format, a...)
+}
+
+func ActionV(w io.Writer, format string) {
+	LogViewF(w, Running, format)
+}
+func SuccessfulVf(w io.Writer, format string) {
+	LogViewF(w, ThumbsUp, format)
+}
+func FailedActionV(w io.Writer, format string) {
+	LogViewF(w, Failure, format)
+}
+func ExitActionV(w io.Writer, format string) {
+	LogViewF(w, Deleted, format)
+}
+func ExitActionVF(w io.Writer, format string, a ...interface{}) {
+	LogViewF(w, Deleted, format, a...)
+}
+func ActionVF(w io.Writer, format string, a ...interface{}) {
+	LogViewF(w, Running, format, a...)
+}
+func SuccessfulActionVF(w io.Writer, format string, a ...interface{}) {
+	LogViewF(w, ThumbsUp, format, a...)
+}
+func FailedActionVF(w io.Writer, format string, a ...interface{}) {
+	LogViewF(w, Failure, format, a...)
+}
+func ActLogCVF(w io.Writer, print bool, format string, a ...interface{}) {
+	if !print {
+		return
+	}
+	LogViewF(w, Running, format, a...)
+}
+
+func OkLogVCF(w io.Writer, print bool, format string, a ...interface{}) {
+	if !print {
+		return
+	}
+	LogViewF(w, ThumbsUp, format, a...)
+}
+
+func ErrorLogVCF(w io.Writer, print bool, format string, a ...interface{}) {
+	if !print {
+		return
+	}
+	LogViewF(w, Failure, format, a...)
+}
 
 func OnReportF(format string, a ...interface{}) {
 	LogF(Documentation, format, a...)
@@ -82,7 +152,10 @@ func Print(st Enum, format string) {
 	msg := String(st, format)
 	fmt.Print(msg)
 }
-
+func LogViewF(w io.Writer, st Enum, format string, a ...interface{}) {
+	msg := StringF(st, format, a...)
+	fmt.Fprintln(w, msg)
+}
 func LogF(st Enum, format string, a ...interface{}) {
 	msg := StringF(st, format, a...)
 	log.Print(msg)
